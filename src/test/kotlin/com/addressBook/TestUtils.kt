b247a4d86f78b2f58257bf16f7cca10d0439df3b
MohammedHamzaMalik <m.h.m.i.malik@gmail.com>
2023-01-20 15:56:57 +0530

    only includes get request functions

---
+++ src/test/kotlin/com/addressBook/TestUtils.kt
@@ -0,0 +1,61 @@
+package com.addressBook
+
+import com.commandPattern.addressBook.dataClasses.Contact
+import com.commandPattern.addressBook.dataClasses.Group
+import com.commandPattern.addressBook.requests.AddContactRequest
+import com.commandPattern.addressBook.requests.EditContactRequest
+import java.util.*
+
+//object TestUtils {
+//    private fun getContact(): Contact {
+//        return Contact(
+//            UUID.randomUUID(),
+//            "Hamza","Malik",
+//            mutableMapOf("work" to "work@gmail.com","home" to "home@gmail.com"),
+//            mutableMapOf("work" to "+91 123","home" to "+91 234"),
+//            mutableMapOf("HOME" to "ST","WORK" to "BRC"),
+//            mutableListOf("Vayana","PDPU")
+//        )
+//    }
+//    fun getGroup(): Group {
+//        return Group(
+//            UUID.randomUUID(),
+//            "Friends",
+//            mutableListOf(getContact())
+//        )
+//    }
+    fun getAddContactRequest(): AddContactRequest {
+        return AddContactRequest(
+            "Hamza","Malik",
+            mutableMapOf("work" to "work@gmail.com","home" to "home@gmail.com"),
+            mutableMapOf("work" to "+91 123","home" to "+91 234"),
+            mutableMapOf("HOME" to "ST","WORK" to "BRC"),
+            mutableListOf("Vayana","PDPU")
+        )
+    }
+//    fun getDeleteContactCommand(contactId: UUID): DeleteContactCommand {
+//        return DeleteContactCommand(getContact().contactId)
+//    }
+
+//    fun getEditContactRequest(): EditContactRequest {
+//        return EditContactRequest(
+//            UUID.randomUUID(),
+//            "John",
+//            "Doe",
+//            mutableMapOf("Personal" to "per@gmail.com", "Work" to "w@gmail.com"),
+//            mutableMapOf("Personal" to "1234567890", "Work" to "0987654321"),
+//            mutableMapOf("Personal" to "123, abc street", "Work" to "456, xyz street"),
+//            mutableListOf("Friends", "Family")
+//        )
+//    }
+//    fun getSearchedContact(): String {
+//        return "Contact Searched"
+//    }
+
+//    fun getAddGroupRequest(): AddGroupRequest {
+//        return AddGroupRequest(
+//            "Friends",
+//            mutableListOf(getContact())
+//        )
+//    }
+//}
